c4a0db5809583ed4b97d155470aed2314d7d1ab4
Himanshu Verma <himanshu.verma@sd2labs.com>
2016-01-06 14:44:17 +0530

    initial-project-version

---
+++ hourly-test.php
@@ -0,0 +1,108 @@
+<html>
+    <head>
+        <link href="http://www.sd2labs.com/wp-content/themes/twentythirteen/css/bootstrap.min.css" rel='stylesheet' type='text/css'/>              
+        <link href="http://www.sd2labs.com/wp-content/themes/twentythirteen/css/style-custom.css" rel='stylesheet' type='text/css'/> 
+    </head>
+    <body>
+<?php
+$html='<div class="row prBdr prTable prTB_Big_Size  method_options" id="sw2">
+    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 noPadd">
+        <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4"><p class="priceHd_3 priceHd_3_1 changeable">Commitment Period</p></div>
+        <div class="col-lg-2 col-md-2 col-sm-2 col-xs-2"><p class="priceHd_3 changeable">Hourly </p></div>
+        <div class="col-lg-2 col-md-2 col-sm-2 col-xs-2"><p class="priceHd_3 changeable">Monthly <span>5%</span></p></div>
+        <div class="col-lg-2 col-md-2 col-sm-2 col-xs-2"><p class="priceHd_3 changeable">Quarterly <span>20%</span></p></div>
+        <div class="col-lg-2 col-md-2 col-sm-2 col-xs-2"><p class="priceHd_3 changeable">Annually <span>25%</span></p></div>
+    </div>
+    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 noPadd">
+        <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4"><p class="priceHd_3 priceHd_3_1 changeable"></p></div>
+        <div class="col-lg-2 col-md-2 col-sm-2 col-xs-2 noPadd priceHd_4 ">
+            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6"><p class="changeable">US/EU</p></div>
+            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6"><p class="changeable">INDIA</p></div>
+        </div>
+        <div class="col-lg-2 col-md-2 col-sm-2 col-xs-2 noPadd priceHd_4 ">
+            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6"><p class="changeable">US/EU</p> </div>
+            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6"><p class="changeable">INDIA</p></div>
+        </div>
+        <div class="col-lg-2 col-md-2 col-sm-2 col-xs-2 noPadd priceHd_4 ">
+            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6"><p class="changeable">US/EU</p></div>
+            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6"><p class="changeable">INDIA</p></div>
+        </div>
+        <div class="col-lg-2 col-md-2 col-sm-2 col-xs-2 noPadd priceHd_4 ">
+            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6"><p class="changeable">US/EU</p></div>
+            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6"><p class="changeable">INDIA</p></div>
+        </div>
+    </div>
+    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 noPadd forHover changeablerow">
+        <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4"><p class="priceHd_3 priceHd_4_1 changeable">Rails / PHP Engineer</p></div>
+        <div class="col-lg-2 col-md-2 col-sm-2 col-xs-2 noPadd priceHd_4 ">
+            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6"><p class="changeable"> 120.00</p></div>
+            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6"><p class="changeable">30.00</p></div>
+        </div>
+        <div class="col-lg-2 col-md-2 col-sm-2 col-xs-2 noPadd priceHd_4 ">
+            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6"><p class="changeable">114.00</p></div>
+            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6"><p class="changeable">28.50</p></div>
+        </div>
+        <div class="col-lg-2 col-md-2 col-sm-2 col-xs-2 noPadd priceHd_4 ">
+            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6"><p class="changeable">96.00</p></div>
+            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6"><p class="changeable">24.00</p></div>
+        </div>
+        <div class="col-lg-2 col-md-2 col-sm-2 col-xs-2 noPadd priceHd_4 ">
+            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6"><p class="changeable">90.00</p></div>
+            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6"><p class="changeable">22.50</p></div>
+        </div>
+    </div><div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 noPadd forHover changeablerow"><div class="col-lg-4 col-md-4 col-sm-4 col-xs-4"><p class="priceHd_3 priceHd_4_1 changeable" onclick="changeformat(this)">--</p></div><div class="col-lg-2 col-md-2 col-sm-2 col-xs-2 noPadd priceHd_4 "><div class="col-lg-6 col-md-6 col-sm-6 col-xs-6"><p class="changeable" onclick="changeformat(this)">35</p></div><div class="col-lg-6 col-md-6 col-sm-6 col-xs-6"><p class="changeable" onclick="changeformat(this)">25</p></div></div><div class="col-lg-2 col-md-2 col-sm-2 col-xs-2 noPadd priceHd_4 "><div class="col-lg-6 col-md-6 col-sm-6 col-xs-6"><p class="changeable" onclick="changeformat(this)">--</p></div><div class="col-lg-6 col-md-6 col-sm-6 col-xs-6"><p class="changeable" onclick="changeformat(this)">--</p></div></div><div class="col-lg-2 col-md-2 col-sm-2 col-xs-2 noPadd priceHd_4 "><div class="col-lg-6 col-md-6 col-sm-6 col-xs-6"><p class="changeable" onclick="changeformat(this)">--</p></div><div class="col-lg-6 col-md-6 col-sm-6 col-xs-6"><p class="changeable" onclick="changeformat(this)">--</p></div></div><div class="col-lg-2 col-md-2 col-sm-2 col-xs-2 noPadd priceHd_4 "><div class="col-lg-6 col-md-6 col-sm-6 col-xs-6"><p class="changeable" onclick="changeformat(this)">--</p></div><div class="col-lg-6 col-md-6 col-sm-6 col-xs-6"><p class="changeable" onclick="changeformat(this)">--</p></div></div></div>
+</div>
+<div class="clearfix"></div>';
+        
+$html=str_replace(' onclick="changeformat(this)"','',$html);
+$rows=array();
+$cos=0;
+//<p class="priceHd_3 priceHd_3_1">....</p>
+preg_match_all("/<p[^>]+class\W+priceHd_3 (.*?)>(.*?)<\/p>/is",$html,$matches,PREG_SET_ORDER);
+
+foreach($matches as $match)
+{
+   if(in_array(trim($match[1]),array('priceHd_3_1 changeable"',"priceHd_3_1 changeable'",'priceHd_4_1 changeable"',"priceHd_4_1 changeable'")))
+   {
+       $rows[count($rows)][0]=$match[2];
+   }
+   elseif(in_array(trim($match[1]),array('changeable"',"changeable'")))
+   {
+       $rows[0][count($rows[0])]=$match[2];
+   }
+}
+preg_match_all("/<p[^>]+class\W+changeable(.*?)>(.*?)<\/p>/is",$html,$secondmatches,PREG_SET_ORDER);
+//echo "<pre>";print_r($matches);echo "</pre>"; 
+//echo "<pre>";print_r($secondmatches);echo "</pre>"; exit;
+
+for($i=1;$i<count($rows);$i++)
+{
+    for($j=1;$j<count($rows[0]);$j++)
+    {
+        for($k=0;$k<2;$k++)
+        {
+            $rows[$i][$j][$k]=$secondmatches[$cos][2];
+            $cos++;
+        }
+       
+    }
+}
+echo "<pre>";print_r($rows);echo "</pre>"; exit;
+$mobile_html="";
+for($col=1;$col<count($rows[0]);$col++)
+{
+    $mobile_html.='<div class="col-xs-12 noPadd"><div class="col-xs-6"><p class="priceHd_3 priceHd_3_1"> '.$rows[0][0].'</p></div><div class="col-xs-6"><p class="priceHd_3">'.$rows[0][$col].'</p></div></div>';
+   $mobile_html.='<div class="col-xs-12 noPadd"><div class="col-xs-6"><p class="priceHd_3 priceHd_3_1">'.$rows[1][0].'</p></div><div class="col-xs-6 noPadd priceHd_4 ">
+   <div class="col-xs-6"><p class="">'.$rows[1][$col][0].'</p></div><div class="col-xs-6"><p class="">'.$rows[1][$col][1].'</p></div></div></div>';
+    $rowhtml='<div class="col-xs-12 noPadd forHover"><div class="col-xs-6"><p class="priceHd_3 priceHd_4_1">__COL_HEADING__</p></div><div class="col-xs-6 noPadd priceHd_4 "><div class="col-xs-6"><p class="">__US_PRICING__</p></div><div class="col-xs-6"><p class="">__INDIA_PRICING__</p></div></div></div>';
+   for($rw=2;$rw<count($rows);$rw++)
+   {
+       $mobile_html.=str_replace(array('__COL_HEADING__','__US_PRICING__','__INDIA_PRICING__'),array($rows[$rw][0],$rows[$rw][$col][0],$rows[$rw][$col][1]),$rowhtml);
+   }
+   // echo $mobile_html.='<div class="clearfix"></div><div class="neImage" style="margin:30px 0px;"><img src="'.get_template_directory_uri().'/images/red-bar-2.jpg" alt="red bar" ></div>';exit;
+}  
+
+
+?>
+    </body>
+</html>
